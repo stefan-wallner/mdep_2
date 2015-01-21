5d6d50deb294e23510acc965245eb3ec3691015e
fkrinner <fabian-krinner@mytum.de>
2015-01-21 09:56:22 +0100

    Bug in anchor_t.cxx fixed

--- anchor_t.cxx
+++ anchor_t.cxx
@@ -1487,7 +1487,7 @@ void anchor_t::write_plots(
 	std::cout<<"write_plots(...): Chi2 for the used paramters is: "<<EvalTbin(tbin,&cpl_all[0],&par[0],&iso[0])<<std::endl;//[0]//
 	for (size_t bin=0;bin<_waveset.nBins();bin++){
 		double mass = _waveset.get_m(bin);
-		double var [] = _waveset.getVar(mass,tbin);
+		double* var = &_waveset.getVar(mass,tbin)[0];
 		std::vector<std::complex<double> > amplitudes = _waveset.amps(var,&cpl_all[0],&par[0],iso_eval);
 		std::complex<double> ancAmp = amplitudes[0];
 		write_out<<mass<<" "<<norm(ancAmp)<<" "<<_data[tbin][bin][0]<<" "<<1/sqrt(_coma[tbin][bin][0][0]);
